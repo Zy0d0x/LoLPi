567ac24ba93b6934db76a24fe631409905610086
Zy0d0x <zy0d0x@nullsecurity.net>
2015-04-01 12:26:54 +0000

    Added LoLStats

---
+++ LoLStats.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+
+import time
+from riotwatcher import RiotWatcher,EUROPE_WEST
+import riotwatcher 
+
+APIKey = 'API KEY HERE'
+
+summoner = ['SUMHERE UIDHERE', 'SUMHERE UIDHERE']
+
+w = RiotWatcher(APIKey)
+
+def wait():
+	while not w.can_make_request():
+		time.sleep(1)
+
+
+def IsPlaying():
+	for summoners in summoner:
+	
+		try:
+	 
+			id = summoners.split()
+			print('Is %s In Game?') %(id[0])
+			currentgame = w.get_current_game(summoner_id=id[1])
+			gameId = currentgame['gameId']
+			
+			gameStartTime = currentgame['gameStartTime']
+			platformId = currentgame['platformId']
+			gameMode = currentgame['gameMode']
+			mapId = currentgame['mapId']
+			gameType = currentgame['gameType']
+			gameQueueConfigId = currentgame['gameQueueConfigId']
+			observers = currentgame['observers']
+			encrpytionKey = currentgame['observers']
+			
+			print "Current Game Stats For %s:\n\nGame Id: %s\nGame Start Time: %s\nPlatform Id: %s\n \
+			Game Mode: %s\nMap Id: %s\nGame Type: %s\nGame Queue Config Id: %s\nObservers: %s\nEncryption Key: %s" \
+			 %(id[0],gameId, gameStartTime, platformId, gameMode, mapId, gameType, gameQueueConfigId, observers, encrpytionKey)	
+			wait()
+		
+		except riotwatcher.LoLException:
+			print('User Is Not In Game')
+
+		except KeyboardInterrupt:
+			print('User Aborted')
+			break	
+
+
+
+def ProfileStats():
+	for summoners in summoner:	
+		try:		
+			name = summoners.split()
+			wait()
+			s = w.get_summoner(name=name[0], region=EUROPE_WEST)
+			profileIconId = s['profileIconId']
+			summonerLevel = s['summonerLevel']
+			revisionDate  = s['revisionDate']
+			revisionDate = str(revisionDate).strip("\r\n\t")
+			id = s['id']
+			name = s['name']
+
+			print "Profile Stats For %s\n\nProfileIconId: %s\nSummonerLevel: %s\nRevisionDate: \
+			%s\nAcount Id: %s\nAcount Name: %s \n\n" %(name,profileIconId,summonerLevel,revisionDate,id,name)
+			wait()
+
+		except riotwatcher.LoLException, Err:
+			print('503 Service unavailable')
+			print Err
+			pass
+
+		except KeyboardInterrupt:
+			print('User Aborted')
+			break
+
+
+
+if __name__ == '__main__':
+	
+	while 1:
+	
+		try:
+	
+			ProfileStats()
+			IsPlaying()
+			print('Waiting 600...')
+			time.sleep(600)
+	
+		except KeyboardInterrupt:
+			print('User Aborted')
+			break
